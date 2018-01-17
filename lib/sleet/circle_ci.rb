8684ce74069701462c729e28313e34aa3a7cc02d
Corey Alexander <coreyja@gmail.com>
2018-01-16 21:37:18 -0500

    Fix auth and add completed query param to the correct place. Use basic auth instead of a query param for auth too

--- lib/sleet/circle_ci.rb
+++ lib/sleet/circle_ci.rb
@@ -10,12 +10,17 @@ module Sleet
       @_token ||= File.read("#{Dir.home}/.circleci.token").strip
     end
 
-    def get(url)
-      Faraday.get(url, circleci_token: token)
+    def get(*args, &block)
+      connection.get(*args, &block)
     end
-
     def self.get(url)
       instance.get(url)
+    end
+
+    private
+
+    def connection
+      @_connection ||= Faraday.new.tap { |c| c.basic_auth(token, '') }
     end
   end
 end
