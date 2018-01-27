6bc94d0e7724055633ab82420af0dc1f5f0650d9
Corey Alexander <coreyja@gmail.com>
2018-01-26 21:51:29 -0500

    :cop:

--- spec/cli/version_spec.rb
+++ spec/cli/version_spec.rb
@@ -1,10 +1,12 @@
 
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 describe 'sleet version', type: :aruba do
   let(:sleet_command_path) { '../../exe/sleet' }
 
   it 'has the correct prefix' do
-    expect(`#{sleet_command_path} version`).to match /^Sleet v/
+    expect(`#{sleet_command_path} version`).to match(/^Sleet v/)
   end
 end
