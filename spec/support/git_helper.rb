1688b54d256f74ff968595da994f698693c457ff
Corey Alexander <coreyja@gmail.com>
2018-02-18 18:51:23 -0500

    Some refactors for the :cop:

--- spec/support/git_helper.rb
+++ spec/support/git_helper.rb
@@ -2,19 +2,12 @@
 
 module GitHelper
   def create_commit(repo)
-    content = "This is a random blob. #{SecureRandom.uuid}"
-    filename = 'README.md'
-    File.write(filename, content)
-    oid = repo.write(content, :blob)
-    index = repo.index
-    index.read_tree(repo.head.target.tree) unless repo.empty?
-    index.add(path: filename, oid: oid, mode: 0o100644)
-    author = { email: 'tanoku@gmail.com', time: Time.now, name: 'Vicent Mart' }
+    index = add_file_to_index(repo)
 
     Rugged::Commit.create(repo,
-                          author: author,
+                          author: fake_author,
                           message: 'Hello world',
-                          committer: author,
+                          committer: fake_author,
                           parents: repo.empty? ? [] : [repo.head.target].compact,
                           tree: index.write_tree(repo),
                           update_ref: 'HEAD')
@@ -26,5 +19,21 @@ module GitHelper
     File.directory?(dirname) || FileUtils.mkdir_p(dirname)
     File.write(path, repo.head.target.tree_id)
     repo.branches[local_branch].upstream = repo.branches[remote_branch]
+  end
+
+  private
+
+  def add_file_to_index(repo)
+    content = "This is a random blob. #{SecureRandom.uuid}"
+    filename = 'README.md'
+    oid = repo.write(content, :blob)
+    index = repo.index
+    index.read_tree(repo.head.target.tree) unless repo.empty?
+    index.add(path: filename, oid: oid, mode: 0o100644)
+    index
+  end
+
+  def fake_author
+    { email: 'email@example.com', time: Time.now, name: 'Person' }
   end
 end
