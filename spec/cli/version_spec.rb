f7a10c5612cbc620a38d415a0b5f528da2987411
Corey Alexander <coreyja@gmail.com>
2020-10-23 15:32:26 -0400

    Add failing specs for purposes of a demo

--- spec/cli/version_spec.rb
+++ spec/cli/version_spec.rb
@@ -10,4 +10,18 @@ describe 'sleet version', type: :cli do
   it 'outputs only the version when given the bare option' do
     expect_command('version --bare').to output(/\d+\.\d+\.\d+/).to_stdout.and without_error
   end
+
+  context 'some specs for the demo' do
+    it 'this one fails' do
+      expect(true).to eq false
+    end
+
+    it 'this one fails too' do
+      expect(1 + 1).to eq 4
+    end
+
+    it 'this one also fails' do
+      expect('a').to eq 'b'
+    end
+  end
 end
